fc6aedc648f30b33ce7027ddcaa0bca661499e83
Tirth <tirthacodes@gmail.com>
2023-07-12 09:07:03 +0545

    project uploaded

---
+++ src/cpu/cpu.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CpuService } from './cpu.service';
+import { PowerModule } from 'src/power/power.module';
+
+
+@Module({
+  providers: [CpuService],
+  imports: [PowerModule],
+  exports: [CpuService]
+})
+export class CpuModule {}
